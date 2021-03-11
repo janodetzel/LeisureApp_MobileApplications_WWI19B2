eebcb5dbd89b8d7469baf09978ba484ebf4f9ad0
who-stel <61997708+who-stel@users.noreply.github.com>
2021-03-11 20:56:12 +0100

    favCard Layout angepasst

--- app/src/main/java/com/example/leisureapp/adapters/FavCardAdapter.java
+++ app/src/main/java/com/example/leisureapp/adapters/FavCardAdapter.java
@@ -20,8 +20,14 @@ public class FavCardAdapter extends BaseAdapter {
     static class ViewHolder{
         public TextView activity;
         public ImageView image;
-        public ImageView iconCosts;
-        public ImageView iconPersons;
+        public ImageView iconCosts1;
+        public ImageView iconCosts2;
+        public ImageView iconCosts3;
+        public ImageView iconCosts4;
+        public ImageView iconPersons1;
+        public ImageView iconPersons2;
+        public ImageView iconPersons3;
+        public ImageView iconPersons4;
         public TextView costs;
         public TextView persons;
     }
@@ -57,20 +63,51 @@ public class FavCardAdapter extends BaseAdapter {
             ViewHolder viewHolder = new ViewHolder();
             viewHolder.activity = (TextView) rowView.findViewById(R.id.favText);
             viewHolder.image = (ImageView) rowView.findViewById(R.id.favPic);
-            viewHolder.iconCosts = (ImageView) rowView.findViewById(R.id.favCostIcon);
-            viewHolder.costs = (TextView) rowView.findViewById(R.id.favCostText);
-            viewHolder.iconPersons = (ImageView) rowView.findViewById(R.id.favPersonsIcon);
-            viewHolder.persons = (TextView) rowView.findViewById(R.id.favPersonsText);
+            viewHolder.iconCosts1 = (ImageView) rowView.findViewById(R.id.favCostIcon1);
+            viewHolder.iconCosts2 = (ImageView) rowView.findViewById(R.id.favCostIcon2);
+            viewHolder.iconCosts3 = (ImageView) rowView.findViewById(R.id.favCostIcon3);
+            viewHolder.iconCosts4 = (ImageView) rowView.findViewById(R.id.favCostIcon4);
+            viewHolder.iconPersons1 = (ImageView) rowView.findViewById(R.id.favPersonsIcon1);
+            viewHolder.iconPersons2 = (ImageView) rowView.findViewById(R.id.favPersonsIcon2);
+            viewHolder.iconPersons3 = (ImageView) rowView.findViewById(R.id.favPersonsIcon3);
+            viewHolder.iconPersons4 = (ImageView) rowView.findViewById(R.id.favPersonsIcon4);
             rowView.setTag(viewHolder);
         }
 
         ViewHolder holder = (ViewHolder) rowView.getTag();
         ItemModel fav = items.get(position);
         holder.activity.setText(""+fav.getActivity());
-        holder.persons.setText(""+fav.getParticipants());
-        holder.costs.setText(""+fav.getPrice());
         //TODO: Change to true url! from FavCard
         holder.image.setImageResource(R.drawable.city_photo);
+
+        double price = fav.getPrice();
+        if (price > 0.0) {
+            holder.iconCosts1.setColorFilter(rowView.getResources().getColor(R.color.blue));
+        }
+        if (price >= 0.25) {
+            holder.iconCosts2.setColorFilter(rowView.getResources().getColor(R.color.blue));
+        }
+        if (price >= 0.5) {
+            holder.iconCosts3.setColorFilter(rowView.getResources().getColor(R.color.blue));
+        }
+        if (price >= 0.75) {
+            holder.iconCosts4.setColorFilter(rowView.getResources().getColor(R.color.blue));
+        }
+
+
+        int persons = fav.getParticipants();
+        if(persons >= 1) {
+            holder.iconPersons1.setColorFilter(rowView.getResources().getColor(R.color.blue));
+        }
+        if(persons >= 2) {
+            holder.iconPersons2.setColorFilter(rowView.getResources().getColor(R.color.blue));
+        }
+        if(persons >= 3) {
+            holder.iconPersons3.setColorFilter(rowView.getResources().getColor(R.color.blue));
+        }
+        if(persons >= 4) {
+            holder.iconPersons4.setColorFilter(rowView.getResources().getColor(R.color.blue));
+        }
 
 
         return rowView;
